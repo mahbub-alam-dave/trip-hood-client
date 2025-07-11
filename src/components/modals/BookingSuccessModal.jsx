34de7f1c07129442fd9b3b830fcc214b573f35ad
mahbub-alam-dave <mahbubalamdave2.00@gmail.com>
2025-07-11 18:45:37 +0600

    modify booking modal

--- src/components/modals/BookingSuccessModal.jsx
+++ src/components/modals/BookingSuccessModal.jsx
@@ -6,7 +6,7 @@ const BookingSuccessModal = ({ isOpen, onClose }) => {
 
   return (
     <div className="fixed inset-0 bg-[var(--color-bg)] dark:bg-[var(--color-bg-dark)] bg-opacity-60 flex justify-center items-center z-50">
-      <div className="bg-white dark:bg-[var(--color-bg-primary-dark)] rounded-2xl p-8 w-[90%] max-w-md text-center shadow-xl relative">
+      <div className="bg-[var(--color-bg-primary)] dark:bg-[var(--color-bg-primary-dark)] rounded-2xl p-8 w-[90%] max-w-md text-center shadow-xl relative">
         
         <FaCheckCircle className="text-green-500 text-5xl mx-auto mb-4" />
         <h2 className="text-2xl font-bold mb-2 text-[var(--color-text-primary)] dark:text-[var(--color-text-primary-two)]">Booking Successful!</h2>
@@ -17,14 +17,14 @@ const BookingSuccessModal = ({ isOpen, onClose }) => {
         <div className="flex flex-col sm:flex-row gap-4">
           <button
             onClick={onClose}
-            className="w-full px-4 py-2 rounded-lg bg-[var(--color-primary)] text-white font-semibold hover:bg-[var(--color-primary-dark)] transition"
+            className="w-full px-4 py-2 rounded-lg bg-[var(--color-primary)] dark:bg-[var(--color-primary-dark)] text-[var(--color-text-primary-two)] font-semibold hover:bg-[var(--color-primary-dark)] transition"
           >
             Confirm Booking
           </button>
 
           <Link
             to="/dashboard/my-bookings"
-            className="w-full px-4 py-2 rounded-lg border border-[var(--color-primary)] text-[var(--color-primary)] font-semibold hover:bg-[var(--color-primary)] hover:text-white transition text-center"
+            className="w-full px-4 py-2 rounded-lg border border-[var(--color-primary)] text-[var(--color-primary)] font-semibold hover:bg-[var(--color-primary)] dark:hover:hover:bg-[var(--color-primary-dark)] hover:text-[var(--color-text-primary-dark)] transition text-center"
           >
             See All Bookings
           </Link>
